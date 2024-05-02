995828cb55e41517edc74be299d7e0a75e17fcdf
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2024-05-02 19:26:45 -0400

    Add stats to results page

--- frontend/buildsmart-toolkit/src/Result.js
+++ frontend/buildsmart-toolkit/src/Result.js
@@ -19,7 +19,17 @@ export function ResultDay() {
       <div>
         <div className="query">
           <h1>Your 1 Day Result</h1>
-          <p>CITY: {location.state.city}</p>
+          <div className="all-stats">
+            <div className="stats" id="location-stat">
+              <h3>Location</h3>
+              <p><b>CITY: </b>{location.state.city}</p>
+              <p><b>STATE: </b>{location.state.state}</p>
+            </div>
+            <div className="stats" id="weather-stat">
+              <h3>Weather Tomorrow</h3>
+              <h1>74°F</h1>
+            </div>
+          </div>
           <div className="chatgpt-answer">
             <p>Hi there! this is chatgpt and i dont know the answer</p>
           </div>
